1279ac80dc2dab6c1b91221ae136d8bb3fc45c6c
AngelVerras <angelverras2006@gmail.com>
2024-12-16 09:03:55 +0200

    Update GCD.c

--- GCD.c
+++ GCD.c
@@ -1,29 +1,40 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
 
-int num0,num1; 
-
-int gcd(int a, int b){ // Συνάρτηση ΜΚΔ Ευκλίδη
-
-  if(b==0){
-    return a;
-  }  else{
-        return gcd(b, a%b);
-  }
+//αναδρομικός αλγόριθμος του Ευκλείδη
+long long int gcd(long long int a, long long int b) {
+    if (b == 0) {
+        return a < 0 ? -a : a; // θετικό αποτέλεσμα
+    } else {
+        return gcd(b, a % b);
+    }
 }
 
-int main(){ //Κύρια Συνάρτηση
-   int num0,num1; 
-    printf("Give the first number: "); // Εισαγωγή πρώτου αριθμού
-    scanf("%d", &num0);
+int main(int argc, char *argv[]) {
+    // Έλεγχος αν ο αριθμός των ορισμάτων είναι σωστός
+    if (argc != 3) {
+        printf("Usage: %s <num1> <num2>\n", argv[0]);
+        return 1;
+    }
 
-    printf("Give the second number: "); // Εισαγωγή δεύτερου αριθμού
-    scanf("%d", &num1);
+    // Μετατροπή των ορισμάτων σε long long int
+    long long int num0 = atoll(argv[1]);
+    long long int num1 = atoll(argv[2]);
 
-    int sum = gcd(num0,num1); // Υπολογισμός συνάρτησης
+    // Έλεγχος για το εύρος των αριθμών
+    if (num0 == 0 || num1 == 0) {
+        return 1; // κωδικός εξόδου 1 αν οποιοσδήποτε αριθμός είναι 0
+    }
+    if (num0 < -1000000000000000000LL || num0 > 1000000000000000000LL || 
+        num1 < -1000000000000000000LL || num1 > 1000000000000000000LL) {
+        printf("Input numbers out of range.\n");
+        return 1;
+    }
 
-    printf("The GCD of %d and %d is: %d\n", num0, num1, sum); // Εμφάνιση αποτελέσματος συνάρτησης
+    // Υπολογισμός του GCD και εμφάνιση του αποτελέσματος
+    long long int result = gcd(num0, num1);
+    printf("gcd(%lld, %lld) = %lld\n", num0, num1, result);
 
     return 0;
-
-
 }
